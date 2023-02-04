c37ec5db5fdb75a1ecd3d038db455345f67853b2
Noibisjunior <noibisjunior22@gmail.com>
2023-02-04 21:13:54 +0100

    added the edit and delete functionality to the my listing section

--- src/Components/ListingItem.jsx
+++ src/Components/ListingItem.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { MdLocationOn } from 'react-icons/md';
+import {FaTrash} from 'react-icons/fa'
+import {MdEdit} from 'react-icons/md'
 
-export default function ListingItem({ listing, id }) {
+export default function ListingItem({ listing, id ,onDelete,onEdit}) {
   return (
     <li
       className="relative bg-white flex flex-col justify-between overflow-hidden
     items-center shadow-md hover:shadow-xl rounded-md transition-shadow
     duration-150 m-[10px]"
     >
-      <Link className='contents' to={`/category/${listing.type}/${id}`}>
+      <Link className="contents" to={`/category/${listing.type}/${id}`}>
         <img
           className="h-[170px] w-full object-cover
          hover:scale-105 transition-scale duration-200
@@ -60,6 +62,22 @@ export default function ListingItem({ listing, id }) {
           </div>
         </div>
       </Link>
+      {onDelete && (
+        <FaTrash
+          className="absolute bottom-2 right-2
+        h-[14px] cursor-pointer text-red-500"
+          onClick={() => onDelete(listing.id)} //calling the ondelete function that is passed as a props to the listing
+        />
+      )}
+
+      {onEdit && (
+        <MdEdit
+          className="absolute bottom-2 right-7
+        h-4 cursor-pointer"
+          onClick={() => onEdit
+          (listing.id)} //calling the ondelete function that is passed as a props to the listing
+        />
+      )}
     </li>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,14 @@
 import { getAuth, updateProfile } from 'firebase/auth';
-import { collection, getDocs,where, orderBy, query, updateDoc } from 'firebase/firestore';
+import {
+  collection,
+  getDocs,
+  where,
+  orderBy,
+  query,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -11,8 +20,8 @@ export default function Profile() {
   const auth = getAuth(); //initializing the auth from firebase
   const Navigate = useNavigate(); // initializing navigate hook
   const [changeDetail, setChangeDetail] = useState('false');
-const [listing,setListings] = useState(null);
-const [loading,setLoading] = useState(true);
+  const [listing, setListings] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -30,49 +39,68 @@ const [loading,setLoading] = useState(true);
       [e.target.id]: e.target.value, // set the prev change to the current change
     }));
   }
-    async function onSubmit() {
-      //This function is returning a promise that is the reason why i use asyn-await
-      try {
-        if (auth.currentUser.displayName !== name) {
-          //checks if the change happens
-          //update displayName in firebase auth
-          await updateProfile(auth.currentUser, {
-            // we need to use await becausse it returns a promise
-            displayName: name,
-          });
+  async function onSubmit() {
+    //This function is returning a promise that is the reason why i use asyn-await
+    try {
+      if (auth.currentUser.displayName !== name) {
+        //checks if the change happens
+        //update displayName in firebase auth
+        await updateProfile(auth.currentUser, {
+          // we need to use await becausse it returns a promise
+          displayName: name,
+        });
 
-          //update name in the firestore
-          const docRef = doc(db, 'users', auth.currentUser.uid);
-          await updateDoc(docRef, {
-            //A Promise resolved once the data has been successfully written to the backend (note that it won't resolve while you're offline)
-            name,
-          });
-        }
-        toast.success('profile details updated');
-      } catch (error) {
-        toast.error('could not update the profile details');
+        //update name in the firestore
+        const docRef = doc(db, 'users', auth.currentUser.uid);
+        await updateDoc(docRef, {
+          //A Promise resolved once the data has been successfully written to the backend (note that it won't resolve while you're offline)
+          name,
+        });
       }
+      toast.success('profile details updated');
+    } catch (error) {
+      toast.error('could not update the profile details');
     }
-  
+  }
 
-  useEffect(() => { //using the useEffect hook to fetch the data from the database
+  useEffect(() => {
+    //using the useEffect hook to fetch the data from the database
     async function fetchUserListings() {
       const listingRef = collection(db, 'listings'); //address of the listing
-      const q = query(listingRef,where("userRef","==",auth.currentUser.uid),orderBy("timestamp","desc"))//query(listingRef,where("userRef
-      const querySnap = await getDocs(q) //passing the query snapshot
-      let listings = []
+      const q = query(
+        listingRef,
+        where('userRef', '==', auth.currentUser.uid),
+        orderBy('timestamp', 'desc')
+      ); //query(listingRef,where("userRef
+      const querySnap = await getDocs(q); //passing the query snapshot
+      let listings = [];
       querySnap.forEach((doc) => {
-       return listings.push({
-        id: doc.id,
-        data: doc.data(),
-    })
-      })
-      setListings(listings) //showing the listings data to the user
-      setLoading(false)
+        return listings.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+      setListings(listings); //showing the listings data to the user
+      setLoading(false);
     }
     fetchUserListings(); //calling fetchUserListings
   }, [auth.currentUser.uid]);
-   //the useeffect is triggered after the user listings have been fetched
+  //the useeffect is triggered after the user listings have been fetched
+  
+  async function onDelete(listingID) {
+    if (window.confirm('Are you sure you want to delete')) {
+      await deleteDoc(doc(db, 'listings', listingID));
+      const updatedListings = listing.filter(
+        (listing) => listing.id !== listingID
+      );
+      setListings(updatedListings);
+      toast.success('successfully deleted the listing');
+    }
+  }
+
+  function onEdit(listingID) {
+    Navigate(`/edit-listing/${listingID}`);
+  }
   return (
     <>
       <section
@@ -157,16 +185,21 @@ const [loading,setLoading] = useState(true);
       <div className="max-w-6xl px-3 mt-6 mx-auto">
         {!loading && listing.length > 0 && (
           <>
-            <h2 className="text 2xl text-center mb-6 font-semibold">My Listing</h2>
+            <h2 className="text 2xl text-center mb-6 font-semibold">
+              My Listing
+            </h2>
             <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6">
               {listing.map((listings) => (
                 <ListingItem
-                  key={ //a new component
+                  key={
+                    //a new component
                     listing.id
                   }
                   id={listing.id}
                   listing={listings.data}
-                /> //passing the props to the list item.
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
+                /> //passing the props to the list item component
               ))}
             </ul>
           </>
@@ -174,4 +207,4 @@ const [loading,setLoading] = useState(true);
       </div>
     </>
   );
-              }
+}
